39337b021bc982a1cb0309bf83c421b06f3aa809
oleksandr.artomkin <oleksandr.artomkin>
2018-04-17 15:11:38 +0300

    first commit

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+/*export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+    { path: 'user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
+    { path: 'table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+    { path: 'typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
+    { path: 'icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
+    { path: 'maps', title: 'Maps',  icon:'pe-7s-map-marker', class: '' },
+    { path: 'notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+    { path: 'upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
+];*/
+
+export const ROUTES: RouteInfo[] = [
+    { path: 'dashboard', title: 'Главная',  icon: 'pe-7s-home', class: '' },
+    { path: 'dashboard', title: 'Команда',  icon: 'pe-7s-users', class: '' },
+    { path: 'user', title: 'Мой профиль',  icon:'pe-7s-id', class: '' },
+    { path: 'table', title: 'Турнирная таблица',  icon:'pe-7s-note2', class: '' },
+    { path: 'typography', title: 'События',  icon:'pe-7s-news-paper', class: '' },
+    { path: 'icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
+    { path: 'notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
