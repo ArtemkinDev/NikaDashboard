9cd20099f8078634dab3adf820727bfcd517b430
oleksandr.artomkin <oleksandr.artomkin>
2018-04-26 17:52:06 +0300

    Add nav picture

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -6,58 +6,64 @@ import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common'
     // moduleId: module.id,
     selector: 'navbar-cmp',
     templateUrl: 'navbar.component.html'
-})
+  })
 
 export class NavbarComponent implements OnInit{
-    private listTitles: any[];
-    location: Location;
-    private toggleButton: any;
-    private sidebarVisible: boolean;
+  private listTitles: any[];
+  location: Location;
+  private toggleButton: any;
+  private sidebarVisible: boolean;
 
-    constructor(location: Location,  private element: ElementRef) {
-      this.location = location;
-          this.sidebarVisible = false;
-    }
+  constructor(location: Location,  private element: ElementRef) {
+    this.location = location;
+    this.sidebarVisible = false;
+  }
 
-    ngOnInit(){
-      this.listTitles = ROUTES.filter(listTitle => listTitle);
-      const navbar: HTMLElement = this.element.nativeElement;
-      this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
-    }
-    sidebarOpen() {
-        const toggleButton = this.toggleButton;
-        const body = document.getElementsByTagName('body')[0];
-        setTimeout(function(){
-            toggleButton.classList.add('toggled');
-        }, 500);
-        body.classList.add('nav-open');
+  ngOnInit(){
+    this.listTitles = ROUTES.filter(listTitle => listTitle);
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0];
+  }
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    setTimeout(function(){
+      toggleButton.classList.add('toggled');
+    }, 500);
+    body.classList.add('nav-open');
 
-        this.sidebarVisible = true;
-    };
-    sidebarClose() {
-        const body = document.getElementsByTagName('body')[0];
-        this.toggleButton.classList.remove('toggled');
-        this.sidebarVisible = false;
-        body.classList.remove('nav-open');
-    };
-    sidebarToggle() {
+    this.sidebarVisible = true;
+  };
+  sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+  };
+  sidebarToggle() {
         // const toggleButton = this.toggleButton;
         // const body = document.getElementsByTagName('body')[0];
         if (this.sidebarVisible === false) {
-            this.sidebarOpen();
+          this.sidebarOpen();
         } else {
-            this.sidebarClose();
+          this.sidebarClose();
         }
-    };
+      };
 
-    getTitle(){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.split('/').pop();
-      for(var item = 0; item < this.listTitles.length; item++){
+      getTitle(){
+        var titlee = this.location.prepareExternalUrl(this.location.path());
+
+        if(titlee.indexOf('player') > -1) {
+          return 'Профиль игрока';
+        } else {
+          titlee = titlee.split('/').pop();
+        }
+
+        for(var item = 0; item < this.listTitles.length; item++){
           if(this.listTitles[item].path === titlee){
-              return this.listTitles[item].title;
-          }
+            return this.listTitles[item].title;
+          } 
+        }
+        return 'Главная';
       }
-      return 'Dashboard';
     }
-}
